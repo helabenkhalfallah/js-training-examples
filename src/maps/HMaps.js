7a2f146fcfcd291b0634df64eb512ab1d4b6a1e3
helabenkhalfallah <hela@upperskills.com>
2018-09-16 17:37:03 +0100

    [js-training-examples] maps & immutable.js

--- src/maps/HMaps.js
+++ src/maps/HMaps.js
@@ -1,5 +1,6 @@
 // es6 Maps concepts
 // Map, WeakMap, Set and WeakSet.
+// http://exploringjs.com/es6/ch_maps-sets.html#sec_overview-maps-sets
 
 const HMaps = () => {
 
--- src/mutable-immutable/HMutability.js
+++ src/mutable-immutable/HMutability.js
@@ -1,6 +1,17 @@
 // es6 Mutability concepts
+// https://facebook.github.io/immutable-js/
+// https://github.com/facebook/immutable-js/
+// https://github.com/facebook/immutable-js/tree/master/__tests__
+// Immutable data cannot be changed once created, leading to much simpler
+// application development, no defensive copying, and enabling advanced
+// memoization and change detection techniques with simple logic.
+// Immutable.js provides many Persistent Immutable data structures
+// including: List, Stack, Map, OrderedMap, Set, OrderedSet and Record
+// Immutable.js also provides a lazy Seq, allowing efficient
+// chaining of collection methods like map and filter without
+// creating intermediate representations. Create some Seq with Range and Repeat.
 
-class HMutability {
+const HMutability = () => {
 
 
 }
